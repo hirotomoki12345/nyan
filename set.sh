bbe6ebb639da8c063dc26aabba8e37cf0252c321
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-05-21 15:58:26 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -3,13 +3,13 @@
 cd aa
 
 
-sudo cp ~/mpg123_transfer/mpg123 /usr/local/bin/
-sudo cp ~/mpg123_transfer/libmpg123.so.0 /usr/local/lib/
-sudo cp ~/mpg123_transfer/libout123.so.0 /usr/local/lib/
-sudo cp ~/mpg123_transfer/libsyn123.so.0 /usr/local/lib/
-sudo cp ~/mpg123_transfer/libm.so.6 /usr/local/lib/
-sudo cp ~/mpg123_transfer/libc.so.6 /usr/local/lib/
-sudo cp ~/mpg123_transfer/ld-linux-aarch64.so.1 /usr/local/lib/
+sudo cp mpg123_transfer/mpg123 /usr/local/bin/
+sudo cp mpg123_transfer/libmpg123.so.0 /usr/local/lib/
+sudo cp mpg123_transfer/libout123.so.0 /usr/local/lib/
+sudo cp mpg123_transfer/libsyn123.so.0 /usr/local/lib/
+sudo cp mpg123_transfer/libm.so.6 /usr/local/lib/
+sudo cp mpg123_transfer/libc.so.6 /usr/local/lib/
+sudo cp mpg123_transfer/ld-linux-aarch64.so.1 /usr/local/lib/
 sudo cp nyan /usr/local/bin/
 sudo cp nyancat /usr/local/bin/
 sudo cp Nyanyanyanyanyanyanya.mp3 /usr/local/etc/
