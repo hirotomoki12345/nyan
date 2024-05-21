49aa2494190a72413a70da70af103b4f72be6092
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-05-21 16:30:51 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 sudo apt install mpg321
 
-curl -o nyancat https://raw.githubusercontent.com/hirotomoki12345/nyan/main/nyancat?token=GHSAT0AAAAAACSPMLHUJHYSJA25CZEVFXRMZSMJQBQ
-curl -o nyan https://raw.githubusercontent.com/hirotomoki12345/nyan/main/nyan?token=GHSAT0AAAAAACSPMLHU4BNNLAUR36JYXX66ZSMJQRA
-curl -o Nyanyanyanyanyanyanya.mp3 https://github.com/hirotomoki12345/nyan/raw/main/Nyanyanyanyanyanyanya.mp3
-
-
-sudo cp Nyanyanyanyanyanyanya.mp3 /usr/local/etc/
-sudo cp nyan /usr/local/bin/
-sudo cp nyancat /usr/local/bin/
-sudo chmod +x /usr/local/bin/nyancat
+git clone https://github.com/hirotomoki12345/nyan.git
+mkdir /usr/local/bin
+cd nyan
+cp nyan /usr/local/bin
+cp nyancat /usr/local/bin
+cp Nyanyanyanyanyanyanya.mp3 /usr/local/bin
