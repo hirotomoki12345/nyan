b43fbf29eb461675a83ca73a33740adb5027c889
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-05-21 16:20:15 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+curl -o aa.zip https://github.com/hirotomoki12345/nyan/raw/main/aa.zip
+unzip aa.zip
+sudo cp Nyanyanyanyanyanyanya.mp3 /usr/local/etc/
+sudo cp nyan /usr/local/bin/
+sudo cp nyancat /usr/local/bin/
