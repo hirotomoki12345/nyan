d508164c74fa976930ddc97ac36906ce803e5823
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-05-21 15:47:16 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,14 +1,5 @@
-curl -o nyan https://raw.githubusercontent.com/hirotomoki12345/repository/main/nyan
-curl -o nyancat https://raw.githubusercontent.com/hirotomoki12345/repository/main/nyancat
-mkdir -p mpg123_transfer
-curl -o mpg123_transfer/ld-linux-aarch64.so.1 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/ld-linux-aarch64.so.1
-curl -o mpg123_transfer/libc.so.6 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/libc.so.6
-curl -o mpg123_transfer/libm.so.6 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/libm.so.6
-curl -o mpg123_transfer/libmpg123.so.0 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/libmpg123.so.0
-curl -o mpg123_transfer/libout123.so.0 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/libout123.so.0
-curl -o mpg123_transfer/libsyn123.so.0 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/libsyn123.so.0
-curl -o mpg123_transfer/mpg123 https://raw.githubusercontent.com/hirotomoki12345/nyan/main/mpg123_transfer/mpg123
-curl -o Nyanyanyanyanyanyanya.mp3 https://github.com/hirotomoki12345/nyan/raw/main/Nyanyanyanyanyanyanya.mp3
+tar -zxvf aa.tar.gz
+cd aa
 
 
 sudo cp ~/mpg123_transfer/mpg123 /usr/local/bin/
@@ -18,5 +9,5 @@ sudo cp ~/mpg123_transfer/libsyn123.so.0 /usr/local/lib/
 sudo cp ~/mpg123_transfer/libm.so.6 /usr/local/lib/
 sudo cp ~/mpg123_transfer/libc.so.6 /usr/local/lib/
 sudo cp ~/mpg123_transfer/ld-linux-aarch64.so.1 /usr/local/lib/
-
-
+sudo cp nyan /usr/local/bin/
+sudo cp nyancat /usr/local/bin/
