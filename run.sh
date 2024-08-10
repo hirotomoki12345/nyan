dd519d8b94db0ff2bc835925ae5640db640d04f3
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-08-10 14:28:43 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,29 @@
-sudo apt install mpg321
+#!/bin/bash
 
+# 必要なパッケージをインストール
+sudo apt update
+sudo apt install -y mpg321 git
+
+# リポジトリをクローン
 git clone https://github.com/hirotomoki12345/nyan.git
-mkdir /usr/local/bin
+
+# /usr/local/bin ディレクトリを作成
+sudo mkdir -p /usr/local/bin
+
+# nyan ディレクトリに移動
 cd nyan
-cp nyan /usr/local/bin
-cp nyancat /usr/local/bin
-cp Nyanyanyanyanyanyanya.mp3 /usr/local/bin
+
+# ファイルを /usr/local/bin にコピー
+sudo cp nyan /usr/local/bin
+sudo cp nyancat /usr/local/bin
+sudo cp Nyanyanyanyanyanyanya.mp3 /usr/local/bin
+
+# 実行権限を付与
 sudo chmod +x /usr/local/bin/nyan
 sudo chmod +x /usr/local/bin/nyancat
+
+# nyan ディレクトリを削除
+cd ..
+rm -rf nyan
+
+echo "セットアップが完了しました。"
