fbbf394ccadd347a7a4eb7872550be4824ce7fd6
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-08-10 17:06:03 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,2 @@
-#!/bin/sh -e
-
-# スクリプトのパス
-SCRIPT_PATH="/usr/local/bin/nyancat.sh"
-
-# 仮想端末番号
-TTY_NUMBER=4
-
-# 仮想端末に切り替え
-/usr/bin/chvt $TTY_NUMBER
-
-# スクリプトを実行
-echo "Starting script on TTY $TTY_NUMBER..."
-# /dev/tty4 にコマンドを送信するために、screen または tmux を使用
-script -c "$SCRIPT_PATH" /dev/tty$TTY_NUMBER
-
-exit 0
+chvt 3
+exec script -c "clear; nyancat" /dev/tty3
